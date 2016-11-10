77cfae4807f71815b3e336df719dd0d2d71a7177
sera.prokop <sera.prokop@gmail.com>
2016-11-10 21:47:04 +0300

    css finish

---
+++ markup/static/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$( document ).ready(function() {
+  
+  // const contact = require('components/contacts/contacts');
+  
+  
+});
+
+
+  
+
+
+
+/*
+    This file can be used as entry point for webpack!
+ */
