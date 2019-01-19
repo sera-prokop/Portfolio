67efbd15aec6dc6da2e6fb0e93d80535fb5ef050
sera.prokop@gmail.com <sera.prokop@gmail.com>
2019-01-19 18:10:05 +0300

    keypress header fix

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,13 +1,19 @@
 module.exports = {
   init: function () {
     var headerLink = $('.header__link, .header__logo a');
-    headerLink.on('click', function (e) {
+    headerLink.on('click keypress', function (e) {
       e.preventDefault();
       var target = this.hash,
           $target = $(target),
           elemTop = $target.offset().top,
           headerHeight = $('.header__wrapper').innerHeight();
       $('html:not(:animated),body:not(:animated)').animate({scrollTop: elemTop - headerHeight}, 750);
+
+      if (e.keyCode === 0 || e.keyCode === 32) {
+        e.preventDefault();
+        $('html:not(:animated),body:not(:animated)').animate({scrollTop: elemTop - headerHeight}, 750);
+      }
+
     });
 
   }
