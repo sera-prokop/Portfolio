682ba6ea00bb4bcb0f0218263fe783b4662e4063
sera.prokop@gmail.com <sera.prokop@gmail.com>
2019-01-19 17:00:44 +0300

    validation form

--- markup/components/contacts/contacts.js
+++ markup/components/contacts/contacts.js
@@ -10,8 +10,96 @@ module.exports = {
       success: function() {
         //код в этом блоке выполняется при успешной отправке сообщения
         alert("Ваше сообщение отпрaвлено!");
+        $('input, textarea').val('');
         }
       });
+    });
+
+
+    $('button[type="submit"]').on('click',function(e) {
+      e.preventDefault();
+
+      var $this = $(this),
+        form = $this.closest('form'),
+        inputs = form.find('input'),
+        length = inputs.length,
+        send = true;
+
+      inputs.each(function() {
+
+        var $this = $(this),
+          value = $this.val(),
+          name = $this.attr('name');
+
+        if (value.length == 0 || $(this).hasClass('error') || name == 'privacy' && $this.prop('checked') == false) {
+
+          if (value.length == 0) {
+            $this.parent().addClass('empty');
+          }
+
+          if (name == 'privacy') {
+            $this.parent().removeClass('error');
+            setTimeout(function() {
+              $this.parent().addClass('error');
+            });
+
+          } else {
+            $this.addClass('error');
+            form.find('.error:first').focus();
+          }
+          send = false;
+        }
+        length -= 1;
+      });
+
+      if (length == 0 && send) {
+
+        form.submit();
+      }
+    });
+
+
+
+
+
+    $('input[name="name"]').on('keyup click input change',function() {
+
+      var $this = $(this),
+        // message = $this.siblings('.message'),
+        value = $this.val(),
+        length = value.length;
+
+      if (length >= 2) {
+        $this.removeClass('error').addClass('valid');
+
+      } else if (length == 0) {
+        $this.removeClass('valid error');
+
+      } else {
+        $this.removeClass('valid').addClass('error');
+      }
+    });
+
+
+    $('input[type="email"]').on('keyup click input change',function(e) {
+
+      var $this = $(this),
+        value = $this.val(),
+        length = value.length,
+
+        dog = value.indexOf('@') + 1,
+        dot = value.indexOf('.') + 1,
+        last = value.substr(length - 1);
+
+      if (length >= 5 && dog >= 2 && dot >= 5 && last != '.' && last != '@') {
+        $this.removeClass('error').addClass('valid');
+
+      } else if (length == 0) {
+        $this.removeClass('valid error');
+
+      } else {
+        $this.removeClass('valid').addClass('error');
+      }
     });
   }
 };
