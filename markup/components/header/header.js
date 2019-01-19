31af12261ccea0d205bef92e17489a1ba4f40f1a
sera.prokop@gmail.com <sera.prokop@gmail.com>
2019-01-19 16:15:03 +0300

    header finish

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,13 +1,13 @@
 module.exports = {
   init: function () {
-    var headerLink = $('.header__link');
+    var headerLink = $('.header__link, .header__logo a');
     headerLink.on('click', function (e) {
-      // e.preventDefault();
-      var target = this.hash;
-      var $target = $(target);
-      var elemTop = $target.offset().top;
-      $('html:not(:animated),body:not(:animated)').animate({scrollTop: elemTop}, 750);
-
+      e.preventDefault();
+      var target = this.hash,
+          $target = $(target),
+          elemTop = $target.offset().top,
+          headerHeight = $('.header__wrapper').innerHeight();
+      $('html:not(:animated),body:not(:animated)').animate({scrollTop: elemTop - headerHeight}, 750);
     });
 
   }
