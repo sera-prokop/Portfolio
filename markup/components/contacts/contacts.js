91fcf7228e67384c8f18c58d7977da51d949c251
Сергей Прокопеня <sera.prokop@gmail.com>
2021-12-03 15:52:04 +0300

    fix bots mail

--- markup/components/contacts/contacts.js
+++ markup/components/contacts/contacts.js
@@ -1,106 +1,116 @@
 module.exports = {
-  init: function () {
-    $('.contacts__form').submit(function (e) {
-    e.preventDefault();
-    var form_data = $(this).serialize();
-    $.ajax({
-      type: "POST", //Метод отправки
-      url: "send.php", //путь до php фаила отправителя
-      data: form_data,
-      success: function() {
-        //код в этом блоке выполняется при успешной отправке сообщения
-        alert("Ваше сообщение отпрaвлено!");
-        $('input, textarea').val('');
-        }
-      });
-    });
+    init: function () {
+        $('.contacts__form').submit(function (e) {
+            e.preventDefault();
+            var form_data = $(this).serialize();
+            console.log(form_data);
+            $.ajax({
+                type: "POST", //Метод отправки
+                url: "send.php", //путь до php фаила отправителя
+                data: form_data,
+                success: function () {
+                    //код в этом блоке выполняется при успешной отправке сообщения
+                    alert("Ваше сообщение отпрaвлено!");
+                    $('input, textarea').val('');
+                }
+            });
+        });
 
 
-    $('button[type="submit"]').on('click',function(e) {
-      e.preventDefault();
+        $('button[type="submit"]').on('click', function (e) {
+            e.preventDefault();
 
-      var $this = $(this),
-        form = $this.closest('form'),
-        inputs = form.find('input'),
-        length = inputs.length,
-        send = true;
+            var $this = $(this),
+                form = $this.closest('form'),
+                inputs = form.find('input'),
+                length = inputs.length,
+                send = true;
 
-      inputs.each(function() {
+            inputs.each(function () {
 
-        var $this = $(this),
-          value = $this.val(),
-          name = $this.attr('name');
+                var $this = $(this),
+                    value = $this.val(),
+                    name = $this.attr('name');
 
-        if (value.length == 0 || $(this).hasClass('error') || name == 'privacy' && $this.prop('checked') == false) {
 
-          if (value.length == 0) {
-            $this.parent().addClass('empty');
-          }
+                if (value.length == 0 || $(this).hasClass('error') || name == 'privacy' && $this.prop('checked') == false) {
 
-          if (name == 'privacy') {
-            $this.parent().removeClass('error');
-            setTimeout(function() {
-              $this.parent().addClass('error');
+                    if (value.length == 0) {
+                        $this.parent().addClass('empty');
+                    }
+
+                    if (name == 'privacy') {
+                        $this.parent().removeClass('error');
+                        setTimeout(function () {
+                            $this.parent().addClass('error');
+                        });
+
+                    } else {
+                        $this.addClass('error');
+                        form.find('.error:first').focus();
+
+                        if($this.prop('name') === 'check'){
+                            $this.removeClass('error').addClass('valid');
+                        }
+                    }
+                    send = false;
+                } else {
+                    send = true;
+                }
+                length -= 1;
             });
 
-          } else {
-            $this.addClass('error');
-            form.find('.error:first').focus();
-          }
-          send = false;
-        }
-        length -= 1;
-      });
+            if (length == 0 && send) {
 
-      if (length == 0 && send) {
+                var elem = e.target;
+                var parent = elem.parentElement;
+                var checkInput = parent.getElementsByClassName("contacts__check")[0];
+                checkInput.value = "secretcode";
 
-        form.submit();
-      }
-    });
+                form.submit();
+            }
+        });
 
 
+        $('input[name="name"]').on('keyup click input change', function () {
+
+            var $this = $(this),
+                // message = $this.siblings('.message'),
+                value = $this.val(),
+                length = value.length;
+
+            if (length >= 2) {
+                $this.removeClass('error').addClass('valid');
+
+            } else if (length == 0) {
+                $this.removeClass('valid error');
+
+            } else {
+                $this.removeClass('valid').addClass('error');
+            }
+        });
 
 
+        $('input[type="email"]').on('keyup click input change', function (e) {
 
-    $('input[name="name"]').on('keyup click input change',function() {
+            var $this = $(this),
+                value = $this.val(),
+                length = value.length,
 
-      var $this = $(this),
-        // message = $this.siblings('.message'),
-        value = $this.val(),
-        length = value.length;
+                dog = value.indexOf('@') + 1,
+                dot = value.indexOf('.') + 1,
+                last = value.substr(length - 1);
 
-      if (length >= 2) {
-        $this.removeClass('error').addClass('valid');
+            if (length >= 5 && dog >= 2 && dot >= 5 && last != '.' && last != '@') {
+                $this.removeClass('error').addClass('valid');
 
-      } else if (length == 0) {
-        $this.removeClass('valid error');
+            } else if (length == 0) {
+                $this.removeClass('valid error');
 
-      } else {
-        $this.removeClass('valid').addClass('error');
-      }
-    });
-
-
-    $('input[type="email"]').on('keyup click input change',function(e) {
-
-      var $this = $(this),
-        value = $this.val(),
-        length = value.length,
-
-        dog = value.indexOf('@') + 1,
-        dot = value.indexOf('.') + 1,
-        last = value.substr(length - 1);
-
-      if (length >= 5 && dog >= 2 && dot >= 5 && last != '.' && last != '@') {
-        $this.removeClass('error').addClass('valid');
-
-      } else if (length == 0) {
-        $this.removeClass('valid error');
-
-      } else {
-        $this.removeClass('valid').addClass('error');
-      }
-    });
-  }
+            } else {
+                $this.removeClass('valid').addClass('error');
+            }
+        });
+    }
 };
 
--- markup/static/misc/send.php
+++ markup/static/misc/send.php
@@ -1,4 +1,5 @@
 <?
+if ($_POST['check'] != 'secretcode') exit('Spam decected');
 if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['mail'])&&$_POST['mail']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
   $to = 'sera.prokop@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
   $subject = 'Обратная связь'; //Загаловок сообщения
@@ -9,8 +10,8 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['mail'])&&$_POST['
                     </head>
                     <body>
                         <p>Имя: '.$_POST['name'].'</p>
-                        <p>E-mail: '.$_POST['mail'].'</p>                        
-                        <p>Комментарий: '.$_POST['comment'].'</p>                        
+                        <p>E-mail: '.$_POST['mail'].'</p>
+                        <p>Комментарий: '.$_POST['comment'].'</p>
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
   $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
